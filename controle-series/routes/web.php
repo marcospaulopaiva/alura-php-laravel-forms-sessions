a177dd5c47ae05e11aeb025ecebf6097bcdfe580
Marcos Paulo <marcos.poo@gmail.com>
2024-08-26 19:59:16 -0300

    Aula 2.1 - Excluindo uma série

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
     return redirect('/series');
 });
 
-Route::resource('/series', SeriesController::class);
+Route::resource('/series', SeriesController::class)
+    ->only(['index','create','store','destroy']);
 
 
