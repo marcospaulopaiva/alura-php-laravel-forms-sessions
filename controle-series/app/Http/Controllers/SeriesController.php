feb20b90d15e8ec567d9bf5ffdb8ae7734bfa2f6
Marcos Paulo <marcos.poo@gmail.com>
2024-08-27 19:13:49 -0300

    Aula 3.1 - Processo de validação

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -23,6 +23,9 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'nome' => ['required','min:3']
+        ]);
         $serie = Serie::create($request->all());
 
         return to_route('series.index')
